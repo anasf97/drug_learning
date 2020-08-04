83214e21ccac9212f82713d2a71e92da887d0f09
anasf97 <anasanchezfernandez1997@gmail.com>
2020-08-04 20:53:17 +0200

    Add github predictions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(name='drug_learning',
       author='Ana Sánchez',
       author_email='ana.sanchez01@estudiant.upf.edu',
       packages=setuptools.find_packages(),
-      install_requires=["numpy", "pandas", "scipy", "tables", "mordred", "pytest"],
+      install_requires=["numpy", "pandas", "scipy", "tables", "pyarrow", "fastparquet", "mordred", "pytest"],
       classifiers=[
        "Programming Language :: Python :: 3",
        "License :: OSI Approved :: MIT License",
